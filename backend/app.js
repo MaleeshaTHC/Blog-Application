0a18c9afcc93154241e2f4b2edf2d07a7edf99c3
MaleeshaTHC <96755@fhss.sjp.ac.lk>
2022-07-08 19:59:51 +0530

    User model, user-conrollers & user-routes created

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,23 @@
 import express from "express";
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
+// import blogRouter from "./routes/blog-routes";
+import router from "./routes/user-routes";
+// import cors from "cors";
+const app = express();
+app.use(express.json());
 
-const app = express()
+app.use("/api/user", router) 
 
-mongoose.connect(
+// app.use(cors());
+
+// app.use("/api/user", router);
+// app.use("/api/blog", blogRouter);
+mongoose
+  .connect(
     "mongodb+srv://BlogWebsite:BlogWebsite@cluster0.4x97p.mongodb.net/Blog?retryWrites=true&w=majority"
-    ).then(() => app.listen(5000))
-    .then(() => console.log('Connected the Database with localhost:5000')).catch((err) => console.log(err))
-app.listen(5000)
-
-
+  )
+  .then(() => app.listen(4000))
+  .then(() =>
+    console.log("Connected TO Database and Listening TO Localhost 4000")
+  )
+  .catch((err) => console.log(err));
