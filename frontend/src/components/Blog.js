8a67e63909be0aa341d604ceaccb94a161c9dab5
MaleeshaTHC <96755@fhss.sjp.ac.lk>
2022-07-09 10:57:11 +0530

    fetch posts of the user

--- frontend/src/components/Blog.js
+++ frontend/src/components/Blog.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import {Card, CardHeader, Avatar, CardMedia, CardContent, Typography} from '@mui/material'
 
-const Blog = () => {
+const Blog = ({title, description, imageURL, userName}) => {
   return (
-    <div>Blog</div>
+    <div><Card sx={{
+      width: "40%",
+      margin: "auto",
+      mt: 2,
+      padding: 2,
+      boxShadow: "5px 5px 10px #ccc",
+      ":hover": {
+        boxShadow: "10px 10px 20px #ccc",
+      },
+    }}>
+    <CardHeader
+      avatar={
+        <Avatar sx={{ bgcolor: 'red'}} aria-label="recipe">
+          {userName}
+        </Avatar>
+      }
+      title={title}
+      subheader="September 14, 2016"
+    />
+    <CardMedia
+      component="img"
+      height="194"
+      image={imageURL}
+      alt="Paella dish"
+    />
+    <CardContent>
+      <Typography variant="body2" color="text.secondary">
+        {description}
+      </Typography>
+    </CardContent>
+  </Card></div>
   )
 }
 
--- frontend/src/components/Blogs.js
+++ frontend/src/components/Blogs.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import Blog from './Blog'
 
 const Blogs = () => {
+  const [blogs, setBlogs] = useState()
+  const sendRequest = async () => {
+    const res = await axios.get("http://localhost:4000/api/blog").catch(err => console.log(err))
+    const data = await res.data
+    return data
+  }
+  useEffect(() => {
+    sendRequest().then((data) => setBlogs(data.blogs))
+  }, [])
+  console.log(blogs)
   return (
-    <div>Blogs</div>
+    <div>{blogs && blogs.map((blog, index) => (
+    <Blog 
+      title={blog.title} 
+      description={blog.description} 
+      imageURL={blog.imageURL} 
+      userName={blog.user.name} 
+    />))}</div>
   )
 }
 
