4ae88408e24d5a4f6e8aa43aa2345c3de1c17b38
MaleeshaTHC <96755@fhss.sjp.ac.lk>
2022-07-09 11:59:02 +0530

    fetch single user blog

--- frontend/src/components/Blogs.js
+++ frontend/src/components/Blogs.js
@@ -14,13 +14,15 @@ const Blogs = () => {
   }, [])
   console.log(blogs)
   return (
-    <div>{blogs && blogs.map((blog, index) => (
-    <Blog 
-      title={blog.title} 
-      description={blog.description} 
-      imageURL={blog.imageURL} 
-      userName={blog.user.name} 
-    />))}</div>
+    <div>
+      {blogs && blogs.map((blog, index) => (
+        <Blog 
+          title={blog.title} 
+          description={blog.description} 
+          imageURL={blog.imageURL} 
+          userName={blog.user.name} 
+        />))}
+    </div>
   )
 }
 
