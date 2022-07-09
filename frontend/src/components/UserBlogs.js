903567f4f8876a5f652dd131094b035efc6a859a
MaleeshaTHC <96755@fhss.sjp.ac.lk>
2022-07-09 11:24:09 +0530

    fetch posts of a single user

--- frontend/src/components/UserBlogs.js
+++ frontend/src/components/UserBlogs.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const UserBlogs = () => {
+  const [user, setUser] = useState();
+  const id = localStorage.getItem("userId");
+  const sendRequest = async () => {
+    const res = await axios
+      .get(`http://localhost:4000/api/blog/user/${id}`)
+      .catch((err) => console.log(err));
+    const data = await res.data;
+    return data;
+  };
+  useEffect(() => {
+    sendRequest().then((data) => setUser(data.user));
+  }, []);
+  console.log(user);
   return (
     <div>UserBlogs</div>
   )
