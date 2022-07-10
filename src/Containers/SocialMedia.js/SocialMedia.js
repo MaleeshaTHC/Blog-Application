dbfc8a2474ed687ffbb2ffdfc3d20b109ea0ccfe
MaleeshaTHC <96755@fhss.sjp.ac.lk>
2022-07-10 15:14:51 +0530

    update containers

---
+++ src/Containers/SocialMedia.js/SocialMedia.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './SocialMedia.css'
+
+function SocialMedia() {
+    return (<>
+        <div className="footer-icon-sidebar">
+            <ul className="navbar-nav">
+                <li className="nav-item inline-block">
+                    <a href="https://github.com/MaleeshaTHC" aria-label="Github" className="nav-link text-github"><i
+                        className="fa fa-github"></i></a></li>
+                <li className="nav-item inline-block"><a href="https://www.linkedin.com/in/chathumini-maleesha-39a016215/" aria-label="LinkedIn" className="nav-link"><i className="fa fa-linkedin"
+                ></i></a>
+                </li>
+            </ul>
+        </div>
+
+
+    </>
+    )
+}
+
+export default SocialMedia
---
+++ src/Containers/SocialMedia.js/SocialMediaHorizontal/SocialMediaHorizontal.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function SocialMediaHorizontal() {
+    return (
+        <div className="mobile-only">
+            <div className="footer-icons">
+                <ul className="navbar-nav ">
+                    <li className="nav-item inline-block">
+                        <a href="https://github.com/MaleeshaTHC" aria-label="Github" className="nav-link text-github">
+                            <i className="fa fa-github"></i>
+                        </a>
+                    </li>
+                    <li className="nav-item inline-block">
+                        <a href="https://www.linkedin.com/in/chathumini-maleesha-39a016215/" aria-label="LinkedIn" className="nav-link">
+                            <i className="fa fa-linkedin"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    )
+}
+
+export default SocialMediaHorizontal
